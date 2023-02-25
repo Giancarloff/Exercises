3eb0ac069cf13fc7c6a070adcaa8c797985212c5
Gian <105215197+Giancarloff@users.noreply.github.com>
2023-02-25 16:33:41 -0300

    Cool

---
+++ ChatGPT Exercises/palindrome.py
@@ -0,0 +1,18 @@
+import typing
+
+def is_palindrome(_string: str) -> bool:
+    only_alphanum = ""
+    for c in _string:
+        if c.isalpha():
+            only_alphanum += c
+
+    only_alphanum = only_alphanum.lower()
+    return only_alphanum == only_alphanum[::-1]
+
+# POST FEEBACK
+
+def is_palindrome(text: typing.Optional[str]) -> bool:
+    only_alphanum = "".join(c.lower() for c in text if c.isalnum())
+
+    return only_alphanum == only_alphanum[::-1]
+
